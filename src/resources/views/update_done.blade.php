5e877301f8cab03f29cd40bf93d88658758d74be
kanai naoki <oakn.leg.tech2025@gmail.com>
2024-11-21 00:50:25 +0900

    予約更新機能

--- src/resources/views/update_done.blade.php
+++ src/resources/views/update_done.blade.php
@@ -9,7 +9,7 @@
     <div class="update_done_content">
         <div class="update_done_txt">
             <h3>変更を受け付けました。</h3>
-            <form class="update_done_form" action="/detail" method="get">
+            <form class="update_done_form" action="/my_page" method="get">
             @csrf
                 <button class="update_done_button">戻る</button>
             </form>
